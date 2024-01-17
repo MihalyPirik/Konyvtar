9291401f046ee7c7cde480c0d04db292907bf98a
Mihaly Pirik <misipirik@gmail.com>
2024-01-17 12:27:03 +0100

    Context

---
+++ Konyvtar/Model/KonyvtarContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konyvtar.Model
+{
+    public class KonyvtarContext : DbContext
+    {
+        public DbSet<Szerzo> Szerzo { get; set; }
+        public DbSet<Konyv> Konyv { get; set; }
+        public DbSet<Kolcsonzes> Kolcsonzes { get; set; }
+        public DbSet<Tanulo> Tanulo { get; set; }
+        public DbSet<Tipus> Tipus { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;database=Konyvtar;uid=root;pwd=;", ServerVersion.AutoDetect("Server=localhost;database=Konyvtar;uid=root;pwd=;"));
+        }
+    }
+}
